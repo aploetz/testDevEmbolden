a2e19707512e5f18093802ed4a75cf9f9de08cb0
Aaron Ploetz <aaron.ploetz@aarons-mbp.mynetworksettings.com>
2024-05-11 10:25:22 -0500

    Initial Commit

---
+++ testDevCrystals/testDevCrystalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  testDevCrystalsApp.swift
+//  testDevCrystals
+//
+//  Created by Aaron Ploetz on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct testDevCrystalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
